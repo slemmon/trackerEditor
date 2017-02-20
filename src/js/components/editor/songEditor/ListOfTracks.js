45611bbc9330021fd0143e9ffe2ead7cc12a4380
Jo Jordens <jo@appsaloon.be>
2017-02-20 11:39:40 +0100

    commit work on drag/drop

--- src/js/components/editor/songEditor/ListOfTracks.js
+++ src/js/components/editor/songEditor/ListOfTracks.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react'
 
 class ListOfTracks extends Component {
+    constructor () {
+        super()
+
+        this.state = {
+            target: null
+        }
+
+        this.handleEnter = this.handleEnter.bind(this)
+    }
+
     componentDidMount() {
-        // all blocks need to be draggable
+        // every li needs to be droppable
+        // block that's dragged away needs to be removed from channel array
+        // block that's added to an li needs to be added to a channel array at the correct position
+        // block that's rearranged needs to be rearranged in a channel array
+
+        // how do we know what index the block gets?
+        // position? compare with blocks left and right?
+        const rowElement = this.rowElement
+        if ( rowElement ) {
+            rowElement.addEventListener('dragenter', this.handleEnter )
+            // rowElement.addEventListener('dragleave', this.handleLeave )
+            // rowElement.addEventListener('dragover', this.asd)
+        }
+    }
+
+    handleEnter (e) {
+        console.log(e)
+        if ( e.target.classList.contains('thingy') ) {
+            console.log('set new target')
+            this.setState({
+                target: e
+            })
+        } else if ( e.target.classList.contains('idk') ) {
+            console.log('clear target')
+            this.setState({
+                target: null
+            })
+        }
     }
 
     render () {
         const tracks = this.props.tracks
         return (
-            <div className="idk">
+            <div
+                className="idk"
+                ref={ e => this.rowElement = e }
+            >
                 {tracks.map( t =>
-                    <span key={t.id} className="thingy" style={{width: t.ticks*2, backgroundColor: t.color}}></span>
+                    <Track
+                        key={t.id}
+                        track={t}
+                    />
                 )}
             </div>
         )
@@ -18,3 +61,53 @@ class ListOfTracks extends Component {
 }
 
 export default ListOfTracks
+
+class Track extends Component {
+    constructor () {
+        super()
+
+        this.state = {
+            imBeingDragged: false
+        }
+
+        this.handleDragStart = this.handleDragStart.bind(this)
+        this.handleDragEnd = this.handleDragEnd.bind(this)
+    }
+
+    componentDidMount() {
+        // all blocks need to be draggable
+        const el = this.element
+        if ( el ) {
+            el.addEventListener('dragstart', this.handleDragStart )
+            el.addEventListener('dragend', this.handleDragEnd )
+            el.draggable = true
+        }
+    }
+
+    handleDragStart (e) {
+        console.log('start')
+        setTimeout(
+            () => this.setState({imBeingDragged: true}),
+            // () => el.classList.add('hidden'),
+            1
+        )
+    }
+
+    handleDragEnd (e) {
+        console.log('stop')
+        this.setState({imBeingDragged: false})
+        // el.classList.remove('hidden')
+    }
+
+    render () {
+        const track = this.props.track
+        return (
+            <span
+                ref={ e => this.element = e }
+                className={`thingy ${this.state.imBeingDragged ? 'beingdragged' : ''}`}
+                style={{width: track.ticks*2, backgroundColor: track.color}}
+            >
+            </span>
+        )
+    }
+}
--- src/js/components/editor/songEditor/SongEditor.js
+++ src/js/components/editor/songEditor/SongEditor.js
@@ -11,22 +11,18 @@ class SongEditor extends Component {
             songString: '',
             showString: false,
             channels: [
-                [{ticks: 10, color: '#abc', id: 0}, {ticks: 18, color: '#edf', id: 1}],[],[],[]
+                [
+                    {ticks: 10, color: '#abc', id: 0},
+                    {ticks: 18, color: '#edf', id: 1},
+                    {ticks: 18, color: '#05a', id: 2},
+                    {ticks: 18, color: '#8da', id: 3},
+                    {ticks: 18, color: '#edc', id: 4}
+                ],[],[],[]
             ]
         }
 
         this.playSong = this.playSong.bind(this)
         this.exportSong = this.exportSong.bind(this)
-    }
-
-    componentDidMount() {
-        // every li needs to be droppable
-        // block that's dragged away needs to be removed from channel array
-        // block that's added to an li needs to be added to a channel array at the correct position
-        // block that's rearranged needs to be rearranged in a channel array
-
-        // how do we know what index the block gets?
-        // position? compare with blocks left and right?
     }
 
     playSong () {
@@ -54,48 +50,31 @@ class SongEditor extends Component {
                 <button onClick={ this.exportSong }>Export song</button>
 
                 <ul className="song-editor-channels">
-                    <li className="song-editor-channels-item">
-                        <div className="song-editor-channels-item-name">
-                            <span><i className="fa fa-music" aria-hidden="true"></i></span>
-                            <span>CH 0</span>
-                        </div>
-                        <div className="song-editor-channels-item-editor">
-                            <ListOfTracks
-                                tracks={state.channels[0]}
-                            />
-                        </div>
-                    </li>
-
-                    <li className="song-editor-channels-item">
-                        <div className="song-editor-channels-item-name">
-                            <span><i className="fa fa-music" aria-hidden="true"></i></span>
-                            <span>CH 1</span>
-                        </div>
-                        <div className="song-editor-channels-item-editor">
-                        </div>
-                    </li>
-
-                    <li className="song-editor-channels-item">
-                        <div className="song-editor-channels-item-name">
-                            <span><i className="fa fa-music" aria-hidden="true"></i></span>
-                            <span>CH 2</span>
-                        </div>
-                        <div className="song-editor-channels-item-editor">
-                        </div>
-                    </li>
-
-                    <li className="song-editor-channels-item">
-                        <div className="song-editor-channels-item-name">
-                            <span><i className="fa fa-music" aria-hidden="true"></i></span>
-                            <span>CH 3</span>
-                        </div>
-                        <div className="song-editor-channels-item-editor">
-                        </div>
-                    </li>
+                    <OneOfTheRows
+                        channel={0}
+                        tracks={state.channels[0]}
+                    />
+                    <OneOfTheRows
+                        channel={1}
+                        tracks={state.channels[1]}
+                    />
+                    <OneOfTheRows
+                        channel={2}
+                        tracks={state.channels[2]}
+                    />
+                    <OneOfTheRows
+                        channel={3}
+                        tracks={state.channels[3]}
+                    />
                 </ul>
 
                 { state.showString ?
-                    <pre>
+                    <pre style={{
+                        textTransform: 'initial',
+                        borderStyle: 'inset',
+                        padding: 2,
+                        backgroundColor: '#f3f3f3'
+                    }}>
                         {this.state.songString || "Nothing to listen to yet."}
                     </pre>
                     :null
@@ -106,3 +85,82 @@ class SongEditor extends Component {
 }
 
 export default SongEditor
+
+// class OneOfTheRows extends Component {
+//     // constructor () {
+//     //     super()
+
+//     //     this.handleEnter = this.handleEnter.bind(this)
+//     //     this.handleLeave = this.handleLeave.bind(this)
+//     // }
+//     // componentDidMount() {
+//     //     // every li needs to be droppable
+//     //     // block that's dragged away needs to be removed from channel array
+//     //     // block that's added to an li needs to be added to a channel array at the correct position
+//     //     // block that's rearranged needs to be rearranged in a channel array
+
+//     //     // how do we know what index the block gets?
+//     //     // position? compare with blocks left and right?
+
+//     //     const rowElement = this.rowElement
+//     //     if ( rowElement ) {
+//     //         rowElement.addEventListener('dragenter', this.handleEnter )
+//     //         // rowElement.addEventListener('dragleave', this.handleLeave )
+//     //         // rowElement.addEventListener('dragover', this.asd)
+//     //     }
+//     // }
+
+//     // asd (e) {
+//     //     console.log(e)
+//     // }
+
+//     // handleEnter (e) {
+//     //     console.log(e)
+//     //     if ( e.target.classList.contains('thingy') ) {
+//     //         this.setState({
+//     //             target: e
+//     //         })
+//     //     } else if ( e.target.classList.contains('') ) {
+//     //         this.setState({
+//     //             target: null
+//     //         })
+//     //     }
+//     // }
+
+//     // // handleLeave (e) {
+//     // //     this.setState({
+//     // //         target: null
+//     // //     })
+//     // // }
+
+//     render () {
+//         const props = this.props
+//         return (
+//             <li className="song-editor-channels-item">
+//             {/*<li className="song-editor-channels-item" ref={ e => this.rowElement = e }>*/}
+//                 <div className="song-editor-channels-item-name">
+//                     <span><i className="fa fa-music" aria-hidden="true"></i></span>
+//                     <span>{`CH ${props.channel}`}</span>
+//                 </div>
+//                 <div className="song-editor-channels-item-editor">
+//                     <ListOfTracks
+//                         tracks={props.tracks}
+//                     />
+//                 </div>
+//             </li>
+//         )
+//     }
+// }
+
+const OneOfTheRows = ({channel, tracks}) =>
+    <li className="song-editor-channels-item">
+        <div className="song-editor-channels-item-name">
+            <span><i className="fa fa-music" aria-hidden="true"></i></span>
+            <span>{`CH ${channel}`}</span>
+        </div>
+        <div className="song-editor-channels-item-editor">
+            <ListOfTracks
+                tracks={tracks}
+            />
+        </div>
+    </li>
