3ca40132cbad41fa1751aae2e207f4059329b484
Jo Jordens <jo@appsaloon.be>
2017-05-12 14:07:12 +0200

    drumeditor redux

--- src/js/components/editor/EditorView.js
+++ src/js/components/editor/EditorView.js
@@ -23,258 +23,12 @@ class Editor extends Component {
         }
 
         this.nextId = 0
-        // this.tempo = 25
-        // this.synth = new SquawkSynth()
-        // try {
-        //     this.output = new WebAudioStream()
-        // }
-        // catch (error) {
-        //     console.log('WebAudioStream not supported')
-        // }
-        // this.emulateSampleRate = 16000.0    // Emulation sample rate
-        // this.converter = new SampleRateConverter(this.emulateSampleRate, this.output.getSampleRate())
-        // // this.playSong()
 
-        // this.toggleNote = this.toggleNote.bind(this)
-        // this.updateTrack = this.updateTrack.bind(this)
-        this.createTheSongArray = this.createTheSongArray.bind(this)
-        this.playSong = this.playSong.bind(this)
-        this.listenForSongEnd = this.listenForSongEnd.bind(this)
-        this.togglePauseSong = this.togglePauseSong.bind(this)
-        this.toggleMuteSong = this.toggleMuteSong.bind(this)
-        this.stopSong = this.stopSong.bind(this)
-        this.changeChannel = this.changeChannel.bind(this)
-        this.playSongAndRepeat = this.playSongAndRepeat.bind(this)
         this.setTempo = this.setTempo.bind(this)
         this.save = this.save.bind(this)
         this.load = this.load.bind(this)
         this.handleFileChange = this.handleFileChange.bind(this)
         this.clearForcedData = this.clearForcedData.bind(this)
-    }
-
-    // toggleNote (trackId, note, row) {
-    //     const tracks = this.state.tracks
-    //     let track,
-    //         trackIndex
-    //     for ( let x = 0, l = tracks.length; x < l; x++ ) {
-    //         track = tracks[x]
-    //         if ( track.id === trackId ) {
-    //             trackIndex = x
-    //             track = Object.assign({}, track)
-    //             track.notes = track.notes.slice()
-    //             break
-    //         }
-    //     }
-
-    //     track.notes[track.ticks - 1 - row] = { active: note }
-    //     const newTracks = this.state.tracks.slice()
-    //     newTracks[trackIndex] = track
-
-    //     let activeTrack = this.state.activeTrack
-    //     if ( this.state.activeTrack.id === trackId )
-    //         activeTrack = track
-
-    //     const newPlayable = this.createTheSongArray(activeTrack)
-    //     if ( this.state.autoplay )
-    //         this.playSong(newPlayable)
-
-    //     this.setState({
-    //         tracks: newTracks,
-    //         activeTrack,
-    //         activeTrackPlayable: newPlayable
-    //     })
-    // }
-
-    // updateTrack (id, newTrack) {
-    //     const tracks = this.state.tracks
-    //     let track,
-    //         trackIndex
-    //     for ( let x = 0, l = tracks.length; x < l; x++ ) {
-    //         track = tracks[x]
-    //         if ( track.id === id ) {
-    //             trackIndex = x
-    //             break
-    //         }
-    //     }
-    //     const newTracks = tracks.slice()
-    //     newTracks[trackIndex] = newTrack
-
-    //     let activeTrack = this.state.activeTrack
-    //     if ( this.state.activeTrack.id === id )
-    //         activeTrack = newTrack
-
-    //     const newPlayable = this.createTheSongArray(activeTrack)
-    //     if ( this.state.autoplay )
-    //         this.playSong(newPlayable)
-
-    //     this.setState({
-    //         tracks: newTracks,
-    //         activeTrack,
-    //         activeTrackPlayable: newPlayable
-    //     })
-    // }
-
-    createTheSongArray (track = this.state.activeTrack, channel = this.state.channel) {
-        const notes = track.notes
-        const drum = track.type === 'drum'
-
-        const noteSequence = createNoteSequence(track)
-
-        const templateSong = [
-
-            2,              // number of tracks
-            0,              // address of track 0
-            0,              // address of track 0
-            3,              // address of track 1
-            0,              // address of track 1
-
-            drum?0:1,              // Channel 0 entry track (PULSE)
-            0,              // Channel 1 entry track (SQUARE)
-            0,              // Channel 2 entry track (TRIANGLE)
-            drum?1:0,              // Channel 3 entry track (NOISE)
-
-
-            "Track 0",      // ticks = 0 / bytes = 3
-            64,             // FX: SET VOLUME: volume = 0
-            0,              // FX: SET VOLUME: volume = 0
-            159,            // FX: STOP CURRENT CHANNEL
-
-
-            "Track 1",
-            157, this.tempo,        // SET song tempo: value = 50
-            64, 48          // FX: SET VOLUME: volume = 48
-
-        ]
-
-        if ( channel !== 0 ) {
-            templateSong[5] = 0
-            templateSong[channel + 5] = 1
-        }
-
-        const completeSong = templateSong.concat(noteSequence, [159])
-
-        return completeSong
-
-    }
-
-    playSong (song=this.state.activeTrackPlayable, forcePlay, keepRepeatOn) {
-        // Initialize player
-        this.player = new SquawkStream(this.emulateSampleRate)
-        this.player.setSource(song)
-        // Build graph [this.player]=>[synth]=>[converter]=>[output]
-        // Output is the sink, and drives/times the entire graph.
-        this.synth.connect(this.player)
-        this.converter.connect(this.synth)
-        this.output.connect(this.converter)
-        // Begin playback (?)
-        this.output.play(forcePlay || this.state.autoplay)
-
-        this.trackPlayPosition()
-        this.listenForSongEnd()
-
-        if ( !keepRepeatOn ) this.setState({repeatIsOn: false})
-        // if ( !keepRepeatOn ) this.repeatIsOn = false
-    }
-
-    playSongAndRepeat () {
-        const repeatIsOn = this.state.repeatIsOn
-
-        if ( repeatIsOn ) {
-            clearInterval(this.listenForSongEndInterval)
-            clearInterval(this.trackPlayPositionInterval)
-            this.output.pause(true)
-            this.setState({repeatIsOn: false})
-            return
-        }
-
-        this.setState({repeatIsOn: true})
-
-        let song = this.state.activeTrackPlayable.slice()
-        song.pop()
-        song.push(254)
-        song[0]++
-
-        const asd = [
-            "Track 2",
-            253,
-            255,
-            1,
-            159
-        ]
-
-        song = song.concat(asd)
-
-        const d = song.join(',')
-        const a = d.slice(d.indexOf('Track 1') + 8, d.indexOf('Track 2') - 1)
-        const q = a.split(',')
-
-        song = [].concat(song.slice(0, 5), [q.length + song[3], 0], song.slice(5))
-
-        song[10] = 2
-
-        this.playSong(song, true, true)
-    }
-
-    listenForSongEnd () {
-        if ( this.listenForSongEndInterval )
-            clearInterval(this.listenForSongEndInterval)
-        this.listenForSongEndInterval = setInterval(
-            () => {
-                if ( this.player.getChannelActiveMute() === 0 ) {
-                    this.output.pause(true)
-
-                    clearInterval(this.listenForSongEndInterval)
-                    clearInterval(this.trackPlayPositionInterval)
-
-                }
-            }, 100)
-    }
-
-    trackPlayPosition () {
-        if ( this.trackPlayPositionInterval )
-            clearInterval(this.trackPlayPositionInterval)
-        this.trackPlayPositionInterval = setInterval(
-            () => {
-                document.dispatchEvent(
-                    new CustomEvent(
-                        'playPosition',
-                        {
-                            detail: {
-                                ticks: this.player.getTickCount()
-                            }
-                        }
-                    )
-                )
-            },
-            5
-        )
-    }
-
-    togglePauseSong () {
-        this.state.repeatIsOn = false
-        const currentState = this.state.autoplay
-        const newState = !currentState
-        this.output.pause(true)
-        // this.output.pause(newState)
-        this.setState({autoplay: newState})
-    }
-
-    toggleMuteSong () {
-        const currentState = this.state.songIsMuted
-        const newState = !currentState
-        this.output.setVolume(newState ? 0 : 1)
-        this.setState({songIsMuted: newState})
-    }
-
-    stopSong () {
-        this.output.pause(true)
-    }
-
-    changeChannel (channel) {
-        this.setState({
-            channel,
-            activeTrackPlayable: this.createTheSongArray(undefined, channel)
-        })
     }
 
     setTempo (tempo) {
@@ -377,7 +131,6 @@ class Editor extends Component {
 
     render () {
         const state = this.state
-        // const activeTrack = state.activeTrack
         const activeTrack = this.props.activeTrack
         return (
             <div>
@@ -397,29 +150,9 @@ class Editor extends Component {
                     <TrackEditor
                         track = { activeTrack }
                     />
-                    /*<TrackEditor
-                        activeTrack = { activeTrack }
-                        toggleNote = { this.toggleNote }
-                        updateTrack = { this.updateTrack }
-                        playSong = { () => this.playSong(undefined, true) }
-                        togglePauseSong = { this.togglePauseSong }
-                        toggleMuteSong = { this.toggleMuteSong }
-                        channel = { state.channel }
-                        changeChannel = { this.changeChannel }
-                        autoplayIsOn = { state.autoplay }
-                        isMuted = { state.songIsMuted }
-                    />*/
                 : activeTrack.type === 'drum' ?
                     <DrumEditor
-                        activeTrack = { activeTrack }
-                        updateTrack = { this.updateTrack }
-                        playSong = { () => this.playSong(undefined, true) }
-                        playSongAndRepeat = { this.playSongAndRepeat }
-                        togglePauseSong = { this.togglePauseSong }
-                        toggleMuteSong = { this.toggleMuteSong }
-                        autoplayIsOn = { state.autoplay }
-                        isMuted = { state.songIsMuted }
-                        repeatIsOn = { state.repeatIsOn }
+                        track = { activeTrack }
                     />
                     :null
                 }
--- src/js/components/editor/drumEditor/DrumEditor.js
+++ src/js/components/editor/drumEditor/DrumEditor.js
@@ -1,222 +1,25 @@
-import React, { Component } from 'react'
-import DrumTable from './DrumTable'
-import DrumEffectSelector from './DrumEffectSelector'
+import { connect } from 'react-redux'
+import DrumEditorView from './DrumEditorView'
 
-class DrumEditor extends Component {
-    constructor (props) {
-        super(props)
-
-        this.state = {
-            currentTicks: this.props.activeTrack.ticks,
-            selectedEffect: 'snare'
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateTrack (trackId, track) {
+            dispatch({
+                type: 'UPDATE_TRACK',
+                trackId,
+                track
+            })
+            dispatch({
+                type: 'SET_ACTIVE_TRACK',
+                track
+            })
         }
-
-        this.changeTrackName = this.changeTrackName.bind(this)
-        this.trackTicksAmount = this.trackTicksAmount.bind(this)
-        this.changeTicksAmount = this.changeTicksAmount.bind(this)
-        this.selectEffect = this.selectEffect.bind(this)
-        this.addEffectAtPosition = this.addEffectAtPosition.bind(this)
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            currentTicks: nextProps.activeTrack.ticks
-        })
-    }
-
-    changeTrackName (e) {
-        const track = Object.assign({}, this.props.activeTrack)
-        const newName = e.target.value
-
-        track.name = newName
-
-        this.props.updateTrack(track.id, track)
-    }
-
-    trackTicksAmount (e) {
-        const newValue = parseInt(e.target.value)
-
-        this.setState({
-            currentTicks: newValue
-        })
-    }
-
-    changeTicksAmount (e) {
-        e.preventDefault()
-
-        const track = Object.assign({}, this.props.activeTrack)
-        const currentTicks = this.state.currentTicks
-        const newValue = currentTicks > 0 && currentTicks < 65 ? currentTicks : currentTicks > 64 ? 64 : 1
-
-        track.ticks = newValue
-
-        const newNotes = this.trimNotes(track.notes, newValue)
-        track.notes = newNotes
-
-        this.props.updateTrack(track.id, track)
-    }
-
-    trimNotes (notes, ticks) {
-        const newNotes = notes.map((note, i) => {
-            const noteTicks = this.getEffectLength(note) - 1
-            if ( i + noteTicks >= ticks )
-                return undefined
-            else
-                return note
-        })
-        return newNotes
-    }
-
-    getEffectLength (effect) {
-        switch (effect) {
-            case 'snare': return 2
-            case 'shake': return 4
-            case 'crash': return 16
-            case 'tick': return 1
-            case 'short_crash': return 8
-        }
-    }
-
-    selectEffect (value) {
-        this.setState({
-            selectedEffect: value
-        })
-    }
-
-    addEffectAtPosition (position) {
-
-        const track = Object.assign({}, this.props.activeTrack)
-        const ticks = track.ticks
-        let effects = track.notes.slice()
-
-        if ( effects[position] !== undefined )
-            effects[position] = undefined
-        else
-            effects = this.updateEffectsList(effects, position)
-
-        track.notes = effects
-
-        this.props.updateTrack(track.id, track)
-    }
-
-    updateEffectsList (effects, position) {
-        const selectedEffect = this.state.selectedEffect
-        const track = this.props.activeTrack
-
-        const effectLength = this.getEffectLength(selectedEffect)
-        // const effectLength = selectedEffect === 'snare' ? 2 : selectedEffect === 'shake' ? 4 : 16
-        if ( position + effectLength > track.ticks )
-            return effects
-
-        effects[position] = selectedEffect
-
-        if ( selectedEffect === 'snare' )
-            // remove next 1
-            effects[position+1] = undefined
-        else if ( selectedEffect === 'shake' )
-            for ( let x = 1; x < 4; x++ )
-                effects[position+x] = undefined
-            // remove next 4
-        else if ( selectedEffect === 'crash' )
-            for ( let x = 1; x < 16; x++ )
-                effects[position+x] = undefined
-            // remove next 16
-        // else if ( selectedEffect === 'crash' )
-        //     for ( let x = 1; x < 16; x++ )
-        //         effects[position+x] = undefined
-        //     // remove next 0
-        else if ( selectedEffect === 'short_crash' )
-            for ( let x = 1; x < 8; x++ )
-                effects[position+x] = undefined
-            // remove next 8
-
-        // remove previous if previous is long enough
-        const indexToStartSlicingFrom = position - 15 < 0 ? 0 : position - 15
-        const previousEffects = effects.slice(indexToStartSlicingFrom, position)
-        const selectLength = previousEffects.length
-
-        let thisEffect
-        for ( let x = 0; x < selectLength; x++ ) {
-            thisEffect = previousEffects[x]
-            if ( thisEffect === 'crash' ) {
-                effects[x+indexToStartSlicingFrom] = undefined
-                break
-            } else if ( thisEffect === 'shake' && selectLength - x <= 3 ) {
-                effects[x+indexToStartSlicingFrom] = undefined
-                break
-            } else if ( thisEffect === 'snare' && selectLength - x === 1 ) {
-                effects[x+indexToStartSlicingFrom] = undefined
-                break
-            } else if ( thisEffect === 'short_crash' && selectLength - x <= 7 ) {
-                effects[x+indexToStartSlicingFrom] = undefined
-                break
-            }
-        }
-
-        return effects
-    }
-
-    render () {
-        const activeTrack = this.props.activeTrack
-        const selectedEffect = this.state.selectedEffect
-
-        return (
-            <div id="drum-editor-container" className={ activeTrack.id === undefined ? 'hidden' : '' }>
-
-                <h5>Drum track editor</h5>
-
-                <div className="editor-info">
-                    <div className="editor-info-row">
-                        <label htmlFor="track-name">Name: </label>
-                        <input id="track-name" onChange={this.changeTrackName} type="text" value={activeTrack.name || ""} />
-                    </div>
-                    <form onSubmit={this.changeTicksAmount} className="editor-info-row">
-                        <label htmlFor="track-ticks">Ticks: </label>
-                        <input id="track-ticks" onChange={this.trackTicksAmount} type="number" min="1" max="64" value={this.state.currentTicks || 0} />
-                        <input type="submit" value="ok" />
-                    </form>
-                </div>
-
-                <div className="editor-play-buttons">
-                    <button onClick={this.props.playSong}>play once</button>
-                    <button onClick={this.props.playSongAndRepeat}>{ `${this.props.repeatIsOn ? 'stop' : 'play'} repeat` }</button>
-                    <button onClick={this.props.togglePauseSong}>{ `autoplay ${this.props.autoplayIsOn ? 'off' : 'on'}` }</button>
-                    <button onClick={this.props.toggleMuteSong}>{ `${this.props.isMuted ? 'un' : ''}mute` }</button>
-                </div>
-
-                <DrumTable notes={activeTrack.notes} ticks={activeTrack.ticks} addEffectAtPosition={this.addEffectAtPosition} repeatIsOn={this.props.repeatIsOn} />
-
-                <div className="drum-selector-container">
-                    <DrumEffectSelector
-                        name = "tick"
-                        selectEffect = {this.selectEffect}
-                        selected = {'tick' === selectedEffect}
-                    />
-                    <DrumEffectSelector
-                        name = "snare"
-                        selectEffect = {this.selectEffect}
-                        selected = {'snare' === selectedEffect}
-                    />
-                    <DrumEffectSelector
-                        name = "shake"
-                        selectEffect = {this.selectEffect}
-                        selected = {'shake' === selectedEffect}
-                    />
-                    <DrumEffectSelector
-                        name = "short_crash"
-                        selectEffect = {this.selectEffect}
-                        selected = {'short_crash' === selectedEffect}
-                    />
-                    <DrumEffectSelector
-                        name = "crash"
-                        selectEffect = {this.selectEffect}
-                        selected = {'crash' === selectedEffect}
-                    />
-                </div>
-
-            </div>
-        )
     }
 }
+
+const DrumEditor = connect(
+    null,
+    mapDispatchToProps
+)(DrumEditorView)
 
 export default DrumEditor
