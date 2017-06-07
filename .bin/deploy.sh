be396e11982154fa8c856423cec687e2d097c976
Jo Jordens <jo@appsaloon.be>
2017-06-07 10:40:40 +0200

    updated deploy.sh

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -19,6 +19,8 @@ cp -r lib/ docs/js/
 
 cp -r src/css/images docs/css/images
 cp -r src/images docs/images
+# copy piano keys image
+cp mockups/sketch01.png docs/images
 
 echo "starting browserify"
 node_modules/.bin/browserify -e src/js/index.js -o "docs/js/app.js" -t [ babelify ]
